9d6a9f26c0d95a555cf5d14af94cbbd10d3a3d36
Bogdan Vancea <bvancea@student.ethz.ch>
2014-10-21 13:53:34 +0200

    #1: Index distribution Added more test cases for the converter.

--- shared/src/test/java/ch/ethz/globis/distindex/mapping/bst/LongArrayKeyConverterTest.java
+++ shared/src/test/java/ch/ethz/globis/distindex/mapping/bst/LongArrayKeyConverterTest.java
@@ -16,6 +16,27 @@ public class LongArrayKeyConverterTest {
         }
         assertEquals(expected, keyString);
 
+        keyString = keyConverter.convert(new long[] { 0L, 0L});
+        expected = "";
+        for (int i = 0; i < 64; i++) {
+            expected += "00";
+        }
+        assertEquals(expected, keyString);
+
+        keyString = keyConverter.convert(new long[] { 0L, -1L});
+        expected = "";
+        for (int i = 0; i < 64; i++) {
+            expected += "01";
+        }
+        assertEquals(expected, keyString);
+
+        keyString = keyConverter.convert(new long[] { -1L, -1L});
+        expected = "";
+        for (int i = 0; i < 64; i++) {
+            expected += "11";
+        }
+        assertEquals(expected, keyString);
+
         keyString = keyConverter.convert(new long[] { 0L, -1L, 0L});
         expected = "";
         for (int i = 0; i < 64; i++) {
