726e3b7f629d6b223bf388f3bafa38771b323814
Bogdan Vancea <bvancea@student.ethz.ch>
2014-10-29 17:27:20 +0100

    #1: Index distribution Added documentation to the client dispatcher.

--- client/src/main/java/ch/ethz/globis/distindex/client/io/ClientRequestDispatcher.java
+++ client/src/main/java/ch/ethz/globis/distindex/client/io/ClientRequestDispatcher.java
@@ -8,10 +8,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Dispatches client side requests to the server and returns the results.
+ *
+ * Additional, it also performs request encoding and response decoding.
+ * @param <K>
+ * @param <V>
+ */
 public class ClientRequestDispatcher<K, V> implements RequestDispatcher<K, V> {
 
+    /** The transport used to send encoded messages to the server. */
     private Transport transport;
+
+    /** Encodes requests into a form that can be sent over the transport to the server. */
     protected RequestEncoder<K, V> encoder;
+
+    /** Decodes the messages received from the server to Response objects. */
     protected ResponseDecoder<K, V> decoder;
 
     public ClientRequestDispatcher(Transport transport, RequestEncoder<K, V> encoder, ResponseDecoder<K, V> decoder) {
@@ -20,6 +32,16 @@ public class ClientRequestDispatcher<K, V> implements RequestDispatcher<K, V> {
         this.decoder = decoder;
     }
 
+    /**
+     * Send a request to the host identified by hostId and return the response.
+     *
+     * The request is first encoded to a byte array which is then sent to the client via the available transport.
+     * The byte array received as a response is the decoded to a Response object and returned to the caller.
+     *
+     * @param hostId                                The destination host identifier.
+     * @param request                               The request to be sent.
+     * @return                                      The decoded response.
+     */
     @Override
     public Response<K, V> send(String hostId, Request request) {
         byte[] requestBytes = encode(request);
@@ -27,6 +49,14 @@ public class ClientRequestDispatcher<K, V> implements RequestDispatcher<K, V> {
         return decoder.decode(responseBytes);
     }
 
+    /**
+     * Send a request to a number of remote hosts. This method works as the previous one, with the exception
+     * that multiple responses are received, decoded and then returned to the caller.
+     *
+     * @param hostIds                               The identifiers of the destination hosts.
+     * @param request                               The request to be sent.
+     * @return                                      The decoded responses.
+     */
     @Override
     public List<Response<K, V>> send(List<String> hostIds, Request request) {
         byte[] requestBytes = encode(request);
