e0bfa82895e819f172b0ff69a72b956a5af56d94
Bogdan Vancea <bvancea@student.ethz.ch>
2014-10-29 16:28:07 +0100

    #1: Index distribution Refactored the client TCP transport code for better readability.

--- client/src/main/java/ch/ethz/globis/distindex/client/io/TCPTransport.java
+++ client/src/main/java/ch/ethz/globis/distindex/client/io/TCPTransport.java
@@ -11,53 +11,122 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Default implementation of the message service using java sockets.
+ * Class responsible for sending byte messages to a remote server and receiving the reply.
+ *
+ * To properly handle large messages, this class adds a simple header containing the size of the message being sent.
+ * Furthermore, when reading the reply from the server, it expects a similar header to be present. *
  */
 public class TCPTransport implements Transport {
 
+    /** A map of host id's and open sockets*/
     private Map<String, Socket> connections = new HashMap<>();
 
+    /**
+     * Send the payload received as an argument to the remote host identified by the hostId received as an argument.
+     *
+     * The byte array received as a response from the server is also returned.
+     *
+     * @param host                          The id of the remote host.
+     * @param payload                       The message to be sent.
+     * @return                              The reply of the server.
+     */
     @Override
     public byte[] sendAndReceive(String host, byte[] payload) {
-        Socket socket = connections.get(host);
-        byte[] initial = new byte[4];
-        byte[] data = null;
-        int bytesRead = 0;
+
+        byte[] response = null;
         try {
+            Socket socket = connections.get(host);
             if (socket == null) {
-                String[] tokens = host.split(":");
-                String hostAddress = tokens[0];
-                int port = Integer.parseInt(tokens[1]);
-
-                socket = new Socket(hostAddress, port);
-                connections.put(host, socket);
+                socket = openNewSocket(host);
             }
-            BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
 
-            out.write(BitUtils.toByteArray(payload.length));
-            out.write(payload);
-            out.flush();
-
-            InputStream in = new BufferedInputStream(socket.getInputStream());
-            while (bytesRead < 4) {
-                bytesRead = in.read(initial, bytesRead, 4 - bytesRead);
-            }
-            int dataSize = ByteBuffer.wrap(initial).getInt();
-            data = new byte[dataSize];
-
-            bytesRead = 0;
-            while (bytesRead < dataSize) {
-                bytesRead += in.read(data, bytesRead, dataSize - bytesRead);
-            }
+            send(socket, payload);
+            response = receive(socket);
         } catch (IOException e) {
             System.err.format("Failed to send message to remote host: %s", host);
             e.printStackTrace();
-        } finally {
-            //closeSocketGracefully(socket);
+        }
+        return response;
+    }
+
+    /**
+     * Send a message to the server through the socket received as an argument.
+     * To support large messages, a header containing the size of the payload is first sent, followed by
+     * the byte[] payload itself.
+     *
+     * @param socket                        The socket to the remote server.
+     * @param payload                       The data to be sent to the remote server.
+     * @throws IOException
+     */
+    private void send(Socket socket, byte[] payload) throws IOException {
+        BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+
+        out.write(BitUtils.toByteArray(payload.length));
+        out.write(payload);
+        out.flush();
+    }
+
+    /**
+     * Receive a message from the server through the socket.
+     * To support large messages, the size of the incoming packet is first read, followed by
+     * as many bytes are specified in the size.
+     *
+     * The remote server is responsible for ensuring that the size header is correct.
+     *
+     * @param socket                        The socket to the remote server.
+     * @return                              The message read. It does not include the size header.
+     * @throws IOException
+     */
+    private byte[] receive(Socket socket) throws IOException {
+        byte[] initial = new byte[4];
+        byte[] data;
+        int bytesRead = 0;
+
+        InputStream in = new BufferedInputStream(socket.getInputStream());
+        while (bytesRead < 4) {
+            bytesRead = in.read(initial, bytesRead, 4 - bytesRead);
+        }
+
+        int dataSize = ByteBuffer.wrap(initial).getInt();
+        data = new byte[dataSize];
+
+        bytesRead = 0;
+        while (bytesRead < dataSize) {
+            bytesRead += in.read(data, bytesRead, dataSize - bytesRead);
         }
         return data;
     }
 
+    /**
+     * Open a new socket to the remote server. It is assumed that no current socket is
+     * open for the host with the hostId received as an argument.
+     *
+     * @param host                          The hostId of the remote server to which a new socket should be opened.
+     * @return                              The newly created socket.
+     * @throws IOException
+     */
+    private Socket openNewSocket(String host) throws IOException {
+        String[] tokens = host.split(":");
+        String hostAddress = tokens[0];
+        int port = Integer.parseInt(tokens[1]);
+
+        Socket socket = new Socket(hostAddress, port);
+        if (connections.containsKey(host)) {
+            throw new IllegalStateException("There already is a socket registered to host: " + host);
+        }
+        connections.put(host, socket);
+        return socket;
+    }
+
+    /**
+     * Send the byte array received as an argument to all of the remote hosts identified by the hostId's received
+     * as arguments and return a list of all of the replies.
+     *
+     * //ToDO send the requests in parallel
+     * @param hosts                         A list of the host identifies.
+     * @param payload                       The message to be sent to ALL hosts.
+     * @return                              A list of the replies.
+     */
     @Override
     public List<byte[]> sendAndReceive(List<String> hosts, byte[] payload) {
         List<byte[]> responses = new ArrayList<>();
@@ -65,15 +134,5 @@ public class TCPTransport implements Transport {
             responses.add(sendAndReceive(host, payload));
         }
         return responses;
-    }
-
-    private void closeSocketGracefully(Socket socket) {
-        if (socket != null) {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
