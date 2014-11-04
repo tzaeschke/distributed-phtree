ddc28d46528a3bd490de414de55c424bfd33ad20
Bogdan Vancea <bvancea@student.ethz.ch>
2014-11-04 14:02:07 +0100

    #1: Index distribution Updated PHFactory to use the proper constructors.

--- client/src/main/java/ch/ethz/globis/distindex/client/pht/PHFactory.java
+++ client/src/main/java/ch/ethz/globis/distindex/client/pht/PHFactory.java
@@ -1,10 +1,6 @@
 package ch.ethz.globis.distindex.client.pht;
 
-import ch.ethz.globis.distindex.client.util.UnsafeUtil;
 import ch.ethz.globis.pht.*;
-import sun.misc.Unsafe;
-
-import java.lang.reflect.Field;
 
 public class PHFactory {
 
@@ -25,46 +21,18 @@ public class PHFactory {
     public <V> PhTreeVD<V> createPHTreeVD(int dim, Class<V> valueClass) {
         PhTreeV<V> proxy = createPHTreeMap(dim, Double.SIZE, valueClass);
 
-        PhTreeVD<V> tree = new PhTreeVD<>(dim);
-        Unsafe unsafe = UnsafeUtil.get();
-        try {
-            Field field = PhTreeVD.class.getDeclaredField("pht");
-            long offset = unsafe.objectFieldOffset(field);
-            unsafe.putObject(tree, offset, proxy);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-
-        return tree;
+        return new PhTreeVD<>(proxy);
     }
 
     public PhTree createPHTreeSet(int dim, int depth) {
-        PhTreeV<Object> backingTree = createPHTreeMap(dim, depth, Object.class);
+        PhTreeV<Object> proxy = createPHTreeMap(dim, depth, Object.class);
 
-        PhTree tree = PhTree.create(dim, depth);
-        Unsafe unsafe = UnsafeUtil.get();
-        try {
-            Field field = PhTreeVProxy.class.getDeclaredField("tree");
-            long offset = unsafe.objectFieldOffset(field);
-            unsafe.putObject(tree, offset, backingTree);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-        return tree;
+        return new PhTreeVProxy(proxy);
     }
 
     public PhTreeRangeD createPHTreeRangeSet(int dim, int depth) {
         PhTree backingTree = createPHTreeSet(2 * dim, depth);
 
-        PhTreeRangeD tree = new PhTreeRangeD(dim);
-        Unsafe unsafe = UnsafeUtil.get();
-        try {
-            Field field = PhTreeRangeD.class.getDeclaredField("pht");
-            long offset = unsafe.objectFieldOffset(field);
-            unsafe.putObject(tree, offset, backingTree);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-        return tree;
+        return new PhTreeRangeD(backingTree);
     }
 }
