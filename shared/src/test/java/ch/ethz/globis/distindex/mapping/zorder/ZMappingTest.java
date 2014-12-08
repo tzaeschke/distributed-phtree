162586c44325cdc922cf412a8ac7ab7720fb5a35
Bogdan Vancea <bvancea@student.ethz.ch>
2014-12-08 11:49:17 +0100

    #1: Index distribution Split tests for the mapping.

--- shared/src/test/java/ch/ethz/globis/distindex/mapping/zorder/ZMappingTest.java
+++ shared/src/test/java/ch/ethz/globis/distindex/mapping/zorder/ZMappingTest.java
@@ -1,5 +1,6 @@
 package ch.ethz.globis.distindex.mapping.zorder;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -51,11 +52,31 @@ public class ZMappingTest {
         System.out.println(hosts);
         assertEquals(2, hosts.size());
 
-//        hosts = mapping.get(new long[]{-1L, 1L}, new long[] {1L, -1L});
-//        System.out.println(hosts);
-//        assertEquals(4, hosts.size());
-
         hosts = mapping.get(new long[]{-1L, -1L}, new long[] {1L, 1L});
+        System.out.println(hosts);
+        assertEquals(4, hosts.size());
+    }
+
+    @Test
+    @Ignore
+    public void testReversedArguments() {
+        /*
+            Both bounds represent the same square, but the second one is not
+             represented in lower-left, upper right manner.
+         */
+        //ToDo handle wrong square bounds
+        int dim = 2;
+        ZMapping mapping = new ZMapping(dim);
+        mapping.add("one");
+        mapping.add("two");
+        mapping.add("three");
+        mapping.add("four");
+        List<String> hosts;
+        hosts = mapping.get(new long[] {-1L, -1L}, new long[]{1L, 1L} );
+        System.out.println(hosts);
+        assertEquals(4, hosts.size());
+
+        hosts = mapping.get(new long[] {-1L, 1L}, new long[]{1L, -1L} );
         System.out.println(hosts);
         assertEquals(4, hosts.size());
     }
