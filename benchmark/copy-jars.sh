71207f48154463f2d6267c5f09ecdee286360cdb
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2015-03-26 14:47:35 +0100

    #1: Index distribution Updated scripts and added some results.

--- benchmark/copy-jars.sh
+++ benchmark/copy-jars.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
-SERVERS=("server-us-1.us-central1-a.robotic-branch-89320" "server-us-3.us-central1-a.robotic-branch-89320")
-CLIENTS=("client-us-1.us-central1-a.robotic-branch-89320" "client-us-2.us-central1-a.robotic-branch-89320" "client-us-3.us-central1-a.robotic-branch-89320")
+SERVERS=("server-us-1.us-central1-a.robotic-branch-89320" "server-us-2.us-central1-a.robotic-branch-89320" "server-us-3.us-central1-a.robotic-branch-89320" "server-us-4.us-central1-a.robotic-branch-89320")
+CLIENTS=("client-us-1.us-central1-a.robotic-branch-89320" "client-us-2.us-central1-a.robotic-branch-89320" "client-us-3.us-central1-a.robotic-branch-89320" "client-us-4.us-central1-a.robotic-branch-89320" "client-us-5.us-central1-a.robotic-branch-89320" "client-us-6.us-central1-a.robotic-branch-89320")
+
 for server in $SERVERS
 do
     scp "../middleware/target/middleware-1.0-SNAPSHOT.jar" $server:
--- benchmark/run.sh
+++ benchmark/run.sh
@@ -6,25 +6,31 @@ POINTS=50000
 ZK_HOST="zk-us-1.us-central1-a.robotic-branch-89320"
 ZK_HOST_IP=$(ssh $ZK_HOST 'curl ipecho.net/plain')
 
-SERVERS=("server-us-1.us-central1-a.robotic-branch-89320" "server-us-3.us-central1-a.robotic-branch-89320")
-CLIENTS=("client-us-1.us-central1-a.robotic-branch-89320" "client-us-2.us-central1-a.robotic-branch-89320" "client-us-3.us-central1-a.robotic-branch-89320")
+SERVERS=("server-us-1.us-central1-a.robotic-branch-89320" "server-us-2.us-central1-a.robotic-branch-89320" "server-us-3.us-central1-a.robotic-branch-89320" "server-us-4.us-central1-a.robotic-branch-89320")
+CLIENTS=("client-us-1.us-central1-a.robotic-branch-89320" "client-us-2.us-central1-a.robotic-branch-89320" "client-us-3.us-central1-a.robotic-branch-89320" "client-us-4.us-central1-a.robotic-branch-89320" "client-us-5.us-central1-a.robotic-branch-89320" "client-us-6.us-central1-a.robotic-branch-89320")
 
-NR_SERVERS=${#SERVERS[@]}
-NR_CLIENTS=${#CLIENTS[@]}
+NR_SERVERS=4
+NR_CLIENTS=6
+
+SERVERS=("${SERVERS[@]:0:$NR_SERVERS}")
+CLIENTS=("${CLIENTS[@]:0:$NR_CLIENTS}")
 
 echo "Threads: " $THREADS
 echo "Points: " $POINTS
-echo "Servers: " $NR_SERVERS
-echo "Clients:" $NR_CLIENTS
+echo "Servers: " $NR_SERVERS $SERVERS
+echo "Clients:" $NR_CLIENTS $CLIENTS
 
+echo "Starting ZooKeeper"
 ssh $ZK_HOST 'sh zkStart.sh';
 
+echo "Starting servers"
 for server in $SERVERS
 do
-    SERVER_IP=$(ssh $server 'curl ipecho.net/plain')
+    SERVER_IP=$(ssh $server 'curl -s ipecho.net/plain')
     ssh $server "java -jar middleware-1.0-SNAPSHOT.jar $SERVER_IP:5050 $ZK_HOST_IP:2181 > server.log &"
 done
 
+echo "Starting clients"
 for client in $CLIENTS
 do
     ssh $client "java -cp benchmarks.jar ch.ethz.globis.distindex.cluster.ClusterInsertionBenchmark $ZK_HOST_IP 2181 $POINTS $THREADS > client.log &"
@@ -32,6 +38,7 @@ done
 
 echo "" > client.log
 
+echo "Waiting for the clients to finish executing"
 sleep 1
 for client in $CLIENTS
 do    
@@ -43,20 +50,29 @@ do
     cat $client.log >> client.log
     cat $client.log | grep "insert" | cut -d"," -f1 > $client.dat
     gnuplot -e "imagefile='./insert-tp-${client}.png'; raw_file='./insert-raw-${client}.csv'; inputfile='${client}.dat'" process_logs.gp       
+    rm $client.log
     rm $client.dat
+    rm insert-raw-$client.csv
 done
 
+echo "Stopping servers..."
 for server in $SERVERS
 do
     for pid in $(ssh $server "ps -ef | grep java" | awk '{print $2}')
     do
-        echo $pid
         ssh $server "kill -9 $pid";
     done
 done
 
+echo "Stopping ZooKeeper"
 ssh $ZK_HOST 'sh zkStop.sh';
 
 REPORT_FILE=experiment_$NR_SERVERS\_$NR_CLIENTS\_$THREADS\_$POINTS.txt
+echo "Computing stats and storing them into "$REPORT_FILE
 
-q -d "," "select count(c1) from client.log group by c1" | tail -n +15 | head -n -15 | histogram.py > $REPORT_FILE
+echo "# Throughput stats #" > $REPORT_FILE  
+q -d "," "select count(c1) from client.log group by c1" | tail -n +15 | head -n -15 | histogram.py >> $REPORT_FILE
+echo "# Response time stats #" >> $REPORT_FILE
+q -d "," "select avg(c4) from client.log group by c1" | tail -n +15 | head -n -15 | histogram.py >> $REPORT_FILE
+
+echo "Done!"
